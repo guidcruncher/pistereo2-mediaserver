82fdf389e6b91fb5e34e04026186a5045ca7c022
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-06-12 21:08:37 +0100

    Added dotenv

--- alsa/librespot/librespot.sh
+++ alsa/librespot/librespot.sh
@@ -6,9 +6,15 @@ export PISTEREO_CLIENTSECRET=""
 if [ -f "/run/secrets/PISTEREO_CLIENTSECRET" ]; then
   export PISTEREO_CLIENTSECRET="$(cat /run/secrets/PISTEREO_CLIENTSECRET)"
 fi
- 
-envsubst < ./config-template.yml \
+
+configfilename="$PISTEREO_CONFIG"/configuration.env
+if [ -f "$configfilename" ]; then
+  dotenv -o -e "$configfilename" -- envsubst < ./config-template.yml \
   > "$PISTEREO_CONFIG"/.librespot/config.yml
+else
+  envsubst < ./config-template.yml \
+  > "$PISTEREO_CONFIG"/.librespot/config.yml
+fi
 
 ./go-librespot --config_dir "$PISTEREO_CONFIG"/.librespot/
 
--- alsa/start.sh
+++ alsa/start.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+export PISTEREO_CLIENTSECRET=""
+configfilename="$PISTEREO_CONFIG"/configuration.env
 
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime 
 echo $TZ > /etc/timezone
-
-export PISTEREO_CLIENTSECRET=""
 
 if [ -f "/run/secrets/PISTEREO_CLIENTSECRET" ]; then
   export PISTEREO_CLIENTSECRET="$(cat /run/secrets/PISTEREO_CLIENTSECRET)"
 fi
 
-pm2 start ./ecosystem.config.js --env $NODE_ENV
+if [ -f "$configfilename" ]; then
+  dotenv -o -e "$configfilename" -- pm2 start ./ecosystem.config.js --env $NODE_ENV
+else
+  pm2 start ./ecosystem.config.js --env $NODE_ENV
+fi
 
 pm2 logs
