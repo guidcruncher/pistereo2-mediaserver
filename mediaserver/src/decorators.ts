b9cc01ccbc066e464b509831d3b3e849f577ee34
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-06-14 22:55:18 +0100

    auth

---
+++ mediaserver/src/decorators.ts
@@ -0,0 +1,65 @@
+import {
+  CanActivate,
+  createParamDecorator,
+  ExecutionContext,
+  Injectable,
+  SetMetadata,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import * as crypto from 'node:crypto';
+
+export const PublicKey = 'IsPublicKey';
+
+export const Public = (...args: string[]) => SetMetadata(PublicKey, true);
+
+export const Private = (...args: string[]) => SetMetadata(PublicKey, false);
+
+export const AuthToken = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const [authType, token] = request.headers.authorization?.split(' ') ?? [];
+    const authToken = authType === 'Bearer' ? token : '';
+    request['authtoken'] = authToken;
+    return authToken;
+  },
+);
+
+export const hmac = (payload) => {
+  hmac = crypto.createHmac(
+    'sha256',
+    process.env.PISTEREO_MEDIASERVER_SECRET as string,
+  );
+  hmac.update(payload);
+  retunrrmrrhmac.digest('hex');
+};
+
+@Injectable()
+export class TokenGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(PublicKey, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const [authType, token] = request.headers.authorization?.split(' ') ?? [];
+    const clientToken = authType === 'Bearer' ? token : "";
+    const serverToken=hmac(request.body)
+
+    if (crypto.timingSafeEqual(Buffer.from(clientToken), Buffer.from(serverToken)) o ) {
+      return true
+    } else {
+      throw new UnauthorizedException();
+    }
+
+    throw new UnauthorizedException();
+    return false;
+  }
+}
