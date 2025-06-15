4468dedf830a63185951f6bf7275d17bf47f9816
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-06-15 02:52:34 +0100

    resiliwjce

--- mediaserver/src/state/state.service.ts
+++ mediaserver/src/state/state.service.ts
@@ -8,22 +8,28 @@ export class State {
 
 export class StateService {
   static loadState() {
-    let file = path.join(process.env.PISTEREO_CONFIG as string, 'state.json');
+    try {
+      let file = path.join(process.env.PISTEREO_CONFIG as string, 'state.json');
 
-    if (fs.existsSync(file)) {
-      return JSON.parse(fs.readFileSync(file, 'utf8'));
-    }
+      if (fs.existsSync(file)) {
+        return JSON.parse(fs.readFileSync(file, 'utf8'));
+      }
+    } catch (err) {}
 
     return undefined;
   }
 
   static saveState(state: State) {
-    let file = path.join(process.env.PISTEREO_CONFIG as string, 'state.json');
-    if (fs.existsSync(file)) {
-      fs.unlinkSync(file);
-    }
+    try {
+      let file = path.join(process.env.PISTEREO_CONFIG as string, 'state.json');
+      if (fs.existsSync(file)) {
+        fs.unlinkSync(file);
+      }
 
-    fs.writeFileSync(file, JSON.stringify(state), 'utf8');
-    return state;
+      fs.writeFileSync(file, JSON.stringify(state), 'utf8');
+      return state;
+    } catch (err) {}
+
+    return undefined;
   }
 }
